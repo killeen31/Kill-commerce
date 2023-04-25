7947044afdd9a809bf416214faf66dbdb7a31b72
killeen31 <j31killeen@gmail.com>
2023-04-25 09:11:57 -0400

    Product.js defined columns

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: dataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+  },
+  product_name: {
+    type: dataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: dataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    },
+  },
+  stock: {
+    type: dataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isNumeric: true,
+    },
+  },
+  category_id: {
+    type: dataTypes.INTEGER,
+    references: {
+      model: 'category',
+      key: 'id',
+    },
+  },
   },
   {
     sequelize,
